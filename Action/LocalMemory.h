01cc7a242c2e38a38aa348fa8ec117bbe7f68fe5
Pojan <40696144+fauzankassim@users.noreply.github.com>
2024-02-08 16:06:38 +0800

    Change BallKeys to double array, Create initialSetup()

--- Action/LocalMemory.h
+++ Action/LocalMemory.h
@@ -9,25 +9,25 @@ int point = 0;
 int silo[5][3];
 
 const char* SiloNamespace[5] = {"S1", "S2", "S3", "S4", "S5"};
-const char* BallKeys[15] = {
-  "S1B1", "S1B2", "S1B3",
-  "S2B1", "S2B2", "S2B3",
-  "S3B1", "S3B2", "S3B3",
-  "S4B1", "S4B2", "S4B3",
-  "S5B1", "S5B2", "S5B3"
+const char* BallKeys[5][3] = {
+  {"S1B1", "S1B2", "S1B3"},
+  {"S2B1", "S2B2", "S2B3"},
+  {"S3B1", "S3B2", "S3B3"},
+  {"S4B1", "S4B2", "S4B3"},
+  {"S5B1", "S5B2", "S5B3"}
 };
 
 class LocalMemory {
   public:
-    LocalMemory() {
+    void initialSetup() {
       read();
-    };
+    }
 
     void read() {
       for (int PositionSilo = 0; PositionSilo < 5; PositionSilo++) {
         preferences.begin(SiloNamespace[PositionSilo], true);
         for (int PositionBall = 0; PositionBall < 3; PositionBall++) {
-          silo[PositionSilo][PositionBall] = preferences.getInt(BallKeys[PositionBall + (3 * PositionSilo)], 0);
+          silo[PositionSilo][PositionBall] = preferences.getInt(BallKeys[PositionSilo][PositionBall], 0);
         }
         preferences.end();
       }
